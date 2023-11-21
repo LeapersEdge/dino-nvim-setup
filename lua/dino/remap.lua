265556f4d7f5eb8665154d15959a7a301129c6e4
Dino <84743074+LeapersEdge@users.noreply.github.com>
2023-11-21 17:01:38 +0100

    removed some mappings

--- lua/dino/remap.lua
+++ lua/dino/remap.lua
@@ -22,11 +22,10 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.api.nvim_set_keymap('i', '{', '{}<Left>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '(', '()<Left>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '[', '[]<Left>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '\"', '\"\"<Left>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '\'', '\'\'<Left>', { noremap = true, silent = true })
+
+vim.keymap.set('i', '(', '()<Left>', { noremap = true, silent = true })
+vim.keymap.set('i', '[', '[]<Left>', { noremap = true, silent = true })
+vim.keymap.set('i', '{', '{}<Left>', { noremap = true, silent = true })
 
 function BuildProject()
   local build_dir = vim.fn.getcwd() .. "/build"
