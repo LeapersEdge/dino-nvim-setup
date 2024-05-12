dbbde064ad47ce89399d313244c9cb0d59a6bf79
Dino <84743074+LeapersEdge@users.noreply.github.com>
2024-05-12 22:45:59 +0200

    Update packer.lua

--- lua/dino/packer.lua
+++ lua/dino/packer.lua
@@ -75,5 +75,5 @@ return require('packer').startup(function(use)
   }
 
   use('theHamsta/nvim-dap-virtual-text')
-  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
 end)
